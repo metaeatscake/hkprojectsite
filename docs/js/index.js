ffb261e8b43990ff94a23e90c8f181b03db7026f
IanDG39 <bigjopoy@gmail.com>
2022-05-01 17:15:26 +0800

    minify the base index.js file, for optimization

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,72 +5,84 @@ M.AutoInit();
  * Custom JS Code
  */
 
-let mainSiteLinks = [
-    {
-        "link":"./index.html",
-        "desc":"Home"
-    },
-    {
-        "link":"./guns.html",
-        "desc":"Guns"
-    },
-    {
-        "link":"./gunmap.html",
-        "desc":"Gun Map"
+/*
+    NAVBAR LINKS
+*/
+    let mainSiteLinks = [
+        {
+            "link":"./index.html",
+            "desc":"Home"
+        },
+        {
+            "link":"./guns.html",
+            "desc":"Guns"
+        },
+        {
+            "link":"./gunmap.html",
+            "desc":"Gun Map"
+        },
+        {
+            "link":"./contact.html",
+            "desc":"Contact"
+        }
+    ];
+
+    let nav = document.querySelector("#main-navlist");
+
+    //Initialize main-navlist
+    for(const item of mainSiteLinks){
+        let li = document.createElement("li");
+        let anchor = document.createElement("a");
+
+        anchor.href = item.link;
+        anchor.innerHTML = item.desc;
+        li.appendChild(anchor);
+
+        nav.appendChild(li);
     }
-];
 
-let nav = document.querySelector("#main-navlist");
+    let drawerSiteLinks = [
+        {
+            "isDivider":false,
+            "link": mainSiteLinks[0].link,
+            "desc": mainSiteLinks[0].desc
+        },
+        {
+            "isDivider":true
+        },
+        {
+            "isDivider":false,
+            "link": mainSiteLinks[1].link,
+            "desc": mainSiteLinks[1].desc
+        },
+        {
+            "isDivider":false,
+            "link": mainSiteLinks[2].link,
+            "desc": mainSiteLinks[2].desc
+        },
+        {
+            "isDividier":false,
+            "link": mainSiteLinks[3].link,
+            "desc": mainSiteLinks[3].desc
+        }
+    ];
 
-//Initialize main-navlist
-for(const item of mainSiteLinks){
-    let li = document.createElement("li");
-    let anchor = document.createElement("a");
+    let navMob = document.querySelector("#mobile-sidenav");
 
-    anchor.href = item.link;
-    anchor.innerHTML = item.desc;
-    li.appendChild(anchor);
+    //Initialize mobile mode navbar drawer
+    for(const item of drawerSiteLinks){
+        let li = document.createElement("li");
+        let anchor = document.createElement("a");
 
-    nav.appendChild(li);
-}
+        if(item.isDivider){
+            li.classList = "divider";
+            navMob.appendChild(li);
+            continue;
+        }
 
-let drawerSiteLinks = [
-    {
-        "isDivider":false,
-        "link": mainSiteLinks[0].link,
-        "desc": mainSiteLinks[0].desc
-    },
-    {
-        "isDivider":true
-    },
-    {
-        "isDivider":false,
-        "link": mainSiteLinks[1].link,
-        "desc": mainSiteLinks[1].desc
-    },
-    {
-        "isDivider":false,
-        "link": mainSiteLinks[2].link,
-        "desc": mainSiteLinks[2].desc
-    }
-];
+        anchor.href = item.link;
+        anchor.innerHTML = item.desc;
+        li.appendChild(anchor);
 
-let navMob = document.querySelector("#mobile-sidenav");
-
-//Initialize mobile mode navbar drawer
-for(const item of drawerSiteLinks){
-    let li = document.createElement("li");
-    let anchor = document.createElement("a");
-
-    if(item.isDivider){
-        li.classList = "divider";
         navMob.appendChild(li);
-        continue;
     }
-
-    anchor.href = item.link;
-    anchor.innerHTML = item.desc;
-    li.appendChild(anchor);
-
-    navMob.appendChild(li);
-}
